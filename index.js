0df7eb1666ec0e139f38d38d3dd8b4016b3ce0f8
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-12 17:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return "The line is currently: " +
-      for (let i = 0; i < katzDeliLine.length; i++) {
-        `${i + 1}. ${katzDeliLine[i]}`
-      }
+    var textBase = "The line is currently: "
+
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      `${i + 1}. ${katzDeliLine[i]}`
+    }
   } else {
     return "The line is currently empty."
   }
