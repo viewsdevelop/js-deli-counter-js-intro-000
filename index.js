ee24952c4094843d8a1a7165b91f3469a023575e
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-12 17:07:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(line, name) {
+  line.push(name);
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var call = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    return call;
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "The line is currently: " +
+      for (let i = 0; i < katzDeliLine.length; i++) {
+        i + 1. katzDeliLine[i]
+      }
+    "
+  }
+}
